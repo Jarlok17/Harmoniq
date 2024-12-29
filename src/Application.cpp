615e18f95904c2e96c8d82e3a22aae44737c041a
Jarlock17 <abilik48@gmail.com>
2024-12-29 14:56:33 +0200

    Modify Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,6 +2,8 @@
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
 
+#include "layer/LayersModel.hpp"
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -13,14 +15,17 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.addImportPath("qrc:/Styles");
 
-    //qmlRegisterType<harmoniq::>("Harmoniq", 1, 0, "");
+    // registre type layer
+    qmlRegisterType<harmoniq::layer::LayersModel>("Harmoniq_backend", 1, 0, "LayersModel");
 
     const QUrl url(QStringLiteral("qrc:/Qml/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-                         if (!obj && url == objUrl)
-                             QCoreApplication::exit(-1);
-                     }, Qt::QueuedConnection);
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated, &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
 
     engine.load(url);
 
