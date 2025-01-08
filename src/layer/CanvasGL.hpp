b46d1e2b515b402e5190d896f6297b0205a58157
Jarlock17 <abilik48@gmail.com>
2025-01-08 18:04:29 +0200

    Modify CanvasGL.hpp

--- src/layer/CanvasGL.hpp
+++ src/layer/CanvasGL.hpp
@@ -14,21 +14,19 @@ class CanvasGL : public QQuickItem
 
         Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor NOTIFY backgroundColorChanged)
         Q_PROPERTY(qreal scale READ scale WRITE setScale NOTIFY scaleChanged)
-        Q_PROPERTY(float opacity READ opacity WRITE setOpacity NOTIFY opacityChanged)
     public:
         explicit CanvasGL(QQuickItem *parent = nullptr);
         explicit CanvasGL(const int &w, const int &h, const QColor &background, QQuickItem *parent = nullptr);
         ~CanvasGL();
 
+        void initializeGL(QOpenGLContext *currentContext);
+
         void setBackgroundColor(const QColor &color);
         void setScale(const qreal &scale);
         void setOpacity(const float &opacity);
 
-        void initializeGL();
-
         qreal scale() const { return m_scale; }
         QColor backgroundColor() const { return m_backgroundColor; }
-        float opacity() const { return m_opacity; }
 
     protected:
         QSGNode *updatePaintNode(QSGNode *node, UpdatePaintNodeData *data) override;
@@ -36,15 +34,13 @@ class CanvasGL : public QQuickItem
     signals:
         void backgroundColorChanged();
         void scaleChanged();
-        void opacityChanged();
 
     private:
         QOpenGLFramebufferObject *m_fbo = nullptr;
         QOpenGLShaderProgram *m_shaderProgram = nullptr;
 
-        QColor m_backgroundColor = QColor(255, 255, 255);
+        QColor m_backgroundColor = QColor(255, 255, 255, 255);
         qreal m_scale = 1.0;
-        float m_opacity = 1.0f;
         QSize m_fboSize;
 
         GLuint m_vao = 0;
