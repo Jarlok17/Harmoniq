1b1a0edcd9d7355684694f9d7e26873bb67c366b
Jarlock17 <abilik48@gmail.com>
2025-01-12 18:28:30 +0200

    Add new file types.hpp

---
+++ src/configs/types.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QColor>
+#include <stdint.h>
+
+typedef void HarVoid;
+typedef char HarChar;
+typedef char *HarCharPtr;
+typedef QString HarString;
+
+typedef QColor HarColor;
+
+typedef int8_t HarInt8;
+typedef int16_t HarInt16;
+typedef int32_t HarInt32;
+typedef int64_t HarInt64;
+
+typedef uint8_t HarUInt8;
+typedef uint16_t HarUInt16;
+typedef uint32_t HarUInt32;
+typedef uint64_t HarUInt64;
+
+typedef float HarFloat;
+typedef double HarDouble;
+
+typedef bool HarBool;
