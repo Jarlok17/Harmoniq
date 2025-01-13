276ddacd0f7b5bdf60090477fe25631d40631015
Jarlock17 <abilik48@gmail.com>
2025-01-13 14:39:08 +0200

    Add new file precompiled.hpp

---
+++ src/configs/precompiled.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "debug.hpp"
+#include "platform.hpp"
+
+// std
+#include <memory>
+
+// Qt headers
+#include <QApplication>
+#include <QColor>
+#include <QFile>
+#include <QGenericMatrix>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+#include <QString>
+#include <QThread>
+#include <QVector>
+
+// Qt with OpengGl headers
+#include <QOpenGLExtraFunctions>
+#include <QOpenGLFramebufferObject>
+#include <QOpenGLShaderProgram>
+#include <QQuickItem>
+#include <QQuickWindow>
+#include <QSGGeometryNode>
+#include <QSGSimpleTextureNode>
