1c8428842eaba6cd3fa9628adca0a6c9ed881a00
Jarlock17 <abilik48@gmail.com>
2025-01-13 15:22:43 +0200

    Add new file LayerManager.hpp

---
+++ src/layer/LayerManager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Layer.hpp"
+
+namespace harmoniq { namespace layer {
+class LayerManager : public QObject
+{
+        Q_OBJECT
+
+    public:
+        explicit LayerManager(QObject *parent = nullptr);
+        LayerManager(const LayerManager &) = delete;
+        LayerManager(LayerManager &&) = delete;
+
+        Q_INVOKABLE void addLayer(const QString &lname, const QColor &background = QColor(255, 255, 255));
+        Q_INVOKABLE void removeLayer(const int &index);
+        Q_INVOKABLE std::shared_ptr<Layer> getCurrentLayer(const int &index) const;
+        Q_INVOKABLE QStringList getLayerNames() const;
+
+    signals:
+        void layersChanged();
+
+    private:
+        QVector<std::shared_ptr<Layer>> m_layers;
+        QStringList m_layerNames;
+};
+}} // namespace harmoniq::layer
