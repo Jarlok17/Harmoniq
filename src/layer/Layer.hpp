60899afa7ba5d92e09342ba3adfe89273c452f07
Jarlock17 <abilik48@gmail.com>
2025-01-21 18:37:02 +0200

    Modify Layer.hpp

--- src/layer/Layer.hpp
+++ src/layer/Layer.hpp
@@ -1,29 +1,44 @@
 #pragma once
 
+#include "../brushes/BrushInfo.hpp"
 #include "../configs/precompiled.hpp"
 
 namespace harmoniq { namespace layer {
 
-class Layer : public QQuickItem
+class Layer : public QQuickPaintedItem
 {
         Q_OBJECT
 
         Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor NOTIFY backgroundColorChanged)
+        Q_PROPERTY(QImage thumbnail READ thumbnail WRITE setThumbnail NOTIFY thumbnailChanged)
 
     public:
-        explicit Layer(QQuickItem *parent = nullptr);
+        explicit Layer(QQuickPaintedItem *parent = nullptr);
+
+        void setBackgroundColor(const QColor &color);
+        void setBrushInfo(const brushes::BrushInfo &info) { m_brushInfo = info; }
+        void setThumbnail(const QImage &thumbnail);
 
         QColor backgroundColor() const { return m_backgroundColor; }
-        void setBackgroundColor(const QColor &color);
+        QImage thumbnail() const { return m_thumbnail; }
+        const brushes::BrushInfo &brushInfo() const { return m_brushInfo; }
 
     signals:
         void backgroundColorChanged();
+        void thumbnailChanged();
 
     protected:
-        QSGNode *updatePaintNode(QSGNode *node, UpdatePaintNodeData *data) override;
+        void paint(QPainter *painter) override;
+        void mousePressEvent(QMouseEvent *event) override;
+        void mouseMoveEvent(QMouseEvent *event) override;
 
     private:
+        QImage m_canvas;
         QColor m_backgroundColor;
+
+        QImage m_thumbnail;
+
+        brushes::BrushInfo m_brushInfo;
 };
 
 }} // namespace harmoniq::layer
