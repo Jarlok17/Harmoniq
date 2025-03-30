1ac5902849611539d837befacd08951624bd84d4
Jarlock17 <abilik48@gmail.com>
2025-03-30 15:46:40 +0300

    Modify LayerManager

--- src/layer/LayerManager.cpp
+++ src/layer/LayerManager.cpp
@@ -18,52 +18,103 @@ QVariant LayerManager::data(const QModelIndex &index, int role) const
     if (!index.isValid() || index.row() < 0 || index.row() >= m_layers.size())
         return QVariant();
 
-    const LayerData &layer = m_layers[index.row()];
+    const auto &layer = m_layers.at(index.row());
 
     switch (role) {
     case NameRole:
         return layer.name;
     case LockedRole:
         return layer.locked;
+    case VisibleRole:
+        return layer.visible;
     case WidthRole:
         return layer.layer->width();
     case HeightRole:
         return layer.layer->height();
     case ColorRole:
         return layer.layer->backgroundColor();
-    case VisibleRole:
-        return layer.visible;
+    case LayerObjectRole:
+        return QVariant::fromValue(layer.layer);
     default:
         return QVariant();
+    }
+}
+
+void LayerManager::mergeLayers(const QString &outputFile)
+{
+    if (m_layers.isEmpty()) {
+        qWarning() << "No layers to merge.";
+        return;
+    }
+
+    QSize canvasSize = QSize(m_layers.first().layer->width(), m_layers.first().layer->height());
+    QImage finalImage(canvasSize, QImage::Format_ARGB32);
+    finalImage.fill(Qt::transparent);
+
+    QPainter painter(&finalImage);
+    painter.setCompositionMode(QPainter::CompositionMode_SourceOver);
+
+    for (const auto &layerData : m_layers) {
+        if (layerData.visible) {
+            QImage layerImage = layerData.layer->image();
+            if (!layerImage.isNull()) {
+                painter.drawImage(0, 0, layerImage);
+            } else {
+                qWarning() << "Layer" << layerData.name << "has null image.";
+            }
+        }
+    }
+
+    painter.end();
+
+    QString filePath = QUrl(outputFile).toLocalFile();
+    if (!finalImage.save(filePath)) {
+        qWarning() << "Failed to save merged image to" << filePath;
+    } else {
+        qDebug() << "Merged image saved to" << filePath;
     }
 }
 
 QHash<int, QByteArray> LayerManager::roleNames() const
 {
     return {
-        {   NameRole,         "name"},
-        { LockedRole,       "locked"},
-        {  WidthRole,   "LayerWidth"},
-        { HeightRole,  "LayerHeight"},
-        {  ColorRole,   "LayerColor"},
-        {VisibleRole, "LayerVisible"}
+        {       NameRole,        "name"},
+        {     LockedRole,      "locked"},
+        {    VisibleRole,     "visible"},
+        {      WidthRole,       "width"},
+        {     HeightRole,      "height"},
+        {      ColorRole,       "color"},
+        {LayerObjectRole, "layerObject"}
     };
 }
 
 void LayerManager::addLayer(const QString &lname, const int &w, const int &h, const QColor &background, const bool &locked)
 {
+    int widthToUse;
+    int heightToUse;
+    if (m_layers.isEmpty()) {
+        layerSize.setWidth(w);
+        layerSize.setHeight(h);
+        widthToUse = w;
+        heightToUse = h;
+    } else {
+        widthToUse = layerSize.width();
+        heightToUse = layerSize.height();
+    }
+
     beginInsertRows(QModelIndex(), m_layers.size(), m_layers.size());
     LayerData newLayer;
     newLayer.name = lname;
     newLayer.locked = locked;
     newLayer.visible = true;
-    newLayer.layer = std::make_shared<Layer>();
-    newLayer.layer->setWidth(static_cast<qreal>(w));
-    newLayer.layer->setHeight(static_cast<qreal>(h));
+
+    newLayer.layer = new Layer();
+    newLayer.layer->setWidth(widthToUse);
+    newLayer.layer->setHeight(heightToUse);
     newLayer.layer->setBackgroundColor(background);
-    newLayer.layer->setVisible(newLayer.visible);
+
     m_layers.append(newLayer);
-    qDebug() << "Шар додано успішно";
+    qDebug() << "LAYER COUNT" << this->rowCount();
     endInsertRows();
 }
 
@@ -72,6 +123,7 @@ void LayerManager::removeLayer(const int &index)
     if (index >= 0 && index < m_layers.size()) {
         if (!m_layers[index].locked) {
             beginRemoveRows(QModelIndex(), index, index);
+            delete m_layers[index].layer;
             m_layers.removeAt(index);
             qDebug() << "Шар видалено успішно: " << index;
             endRemoveRows();
@@ -88,6 +140,7 @@ void LayerManager::moveLayer(const int &from, const int &to)
     beginMoveRows(QModelIndex(), from, from, QModelIndex(), to > from ? to + 1 : to);
     m_layers.move(from, to);
     endMoveRows();
+
     qDebug() << "Шар переміщено з " << from << " на " << to;
 }
 
@@ -97,6 +150,21 @@ bool LayerManager::isLayerLocked(const int &index) const
         return m_layers[index].locked;
     }
     return false;
+}
+
+void LayerManager::setCurrentTool(ToolType tool)
+{
+    if (m_currentTool != tool) {
+        m_currentTool = tool;
+
+        for (auto &layerData : m_layers) {
+            if (layerData.layer) {
+                layerData.layer->setCurrentTool(tool);
+            }
+        }
+
+        emit toolChanged(tool);
+    }
 }
 
 void LayerManager::setLayerLocked(const int &index, const bool &locked)
@@ -111,16 +179,7 @@ void LayerManager::setLayerVisible(const int &index, const bool &visible)
 {
     if (index >= 0 && index < m_layers.size()) {
         m_layers[index].visible = visible;
-        m_layers[index].layer->setVisible(visible);
         emit dataChanged(this->index(index), this->index(index), {VisibleRole});
-    }
-}
-
-void LayerManager::setLayerColor(const int &index, const QColor &color)
-{
-    if (index >= 0 && index < m_layers.size()) {
-        m_layers[index].layer->setBackgroundColor(color);
-        emit dataChanged(this->index(index), this->index(index), {ColorRole});
     }
 }
 }} // namespace harmoniq::layer
--- src/layer/LayerManager.hpp
+++ src/layer/LayerManager.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <QQmlListProperty>
+
 #include "../configs/precompiled.hpp"
 #include "Layer.hpp"
 
@@ -12,19 +14,21 @@ class LayerManager : public QAbstractListModel
         enum LayerRoles {
             NameRole = Qt::UserRole + 1,
             LockedRole,
+            VisibleRole,
             WidthRole,
             HeightRole,
             ColorRole,
-            VisibleRole
+            LayerObjectRole,
         };
 
         explicit LayerManager(QObject *parent = nullptr);
         ~LayerManager();
 
-        int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-        QHash<int, QByteArray> roleNames() const override;
+        Q_INVOKABLE int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+        Q_INVOKABLE QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+        Q_INVOKABLE QHash<int, QByteArray> roleNames() const override;
 
+        Q_INVOKABLE void mergeLayers(const QString &outputFile);
         Q_INVOKABLE void addLayer(const QString &lname, const int &w, const int &h, const QColor &background,
                                   const bool &locked = false);
         Q_INVOKABLE void removeLayer(const int &index);
@@ -32,7 +36,13 @@ class LayerManager : public QAbstractListModel
         Q_INVOKABLE bool isLayerLocked(const int &index) const;
         Q_INVOKABLE void setLayerLocked(const int &index, const bool &locked);
         Q_INVOKABLE void setLayerVisible(const int &index, const bool &visible);
-        Q_INVOKABLE void setLayerColor(const int &index, const QColor &color);
+
+        Q_INVOKABLE void setCurrentTool(ToolType tool);
+        Q_INVOKABLE ToolType currentTool() const { return m_currentTool; }
+
+    signals:
+        void layersChanged();
+        void toolChanged(ToolType tool);
 
     private:
         struct LayerData
@@ -40,8 +50,10 @@ class LayerManager : public QAbstractListModel
                 QString name;
                 bool locked = false;
                 bool visible = true;
-                std::shared_ptr<Layer> layer;
+                Layer *layer;
         };
-        QVector<LayerData> m_layers;
+        QList<LayerData> m_layers;
+        QSize layerSize;
+        ToolType m_currentTool = ToolType::None;
 };
 }} // namespace harmoniq::layer
