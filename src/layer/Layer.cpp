380ab96c26cd75aa92ee07df7fa7febe0e3f8ca3
Jarlock17 <abilik48@gmail.com>
2025-03-30 15:46:46 +0300

    Modify Layer

--- src/layer/Layer.cpp
+++ src/layer/Layer.cpp
@@ -1,20 +1,78 @@
 #include "Layer.hpp"
 
-#include "LayerRenderer.hpp"
-
 namespace harmoniq { namespace layer {
 
-QQuickFramebufferObject::Renderer *Layer::createRenderer() const
+Layer::Layer(QQuickItem *parent) : QQuickPaintedItem(parent)
 {
-    auto renderer = new LayerRenderer();
-    renderer->setBackgroundColor(m_backgroundColor);
-    return renderer;
+    setAcceptedMouseButtons(Qt::LeftButton);
+    setAcceptHoverEvents(true);
+    setAcceptTouchEvents(true);
+    setFlag(QQuickItem::ItemHasContents);
+
+    qDebug() << "LAYER: " << this;
+}
+
+Layer::~Layer() { qDebug() << "DESTROY LAYER: " << this; }
+
+void Layer::updateCanvasSize()
+{
+    if (width() <= 0 || height() <= 0)
+        return;
+
+    if (!m_initialized || m_canvas.size() != QSize(width(), height())) {
+        m_canvas = QImage(width(), height(), QImage::Format_ARGB32);
+        m_canvas.fill(m_backgroundColor.isValid() ? m_backgroundColor : Qt::transparent);
+        m_initialized = true;
+        update();
+    }
+}
+
+void Layer::geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry)
+{
+    QQuickPaintedItem::geometryChange(newGeometry, oldGeometry);
+    updateCanvasSize();
+}
+
+void Layer::paint(QPainter *painter)
+{
+    if (m_visible) {
+        if (!m_initialized)
+            updateCanvasSize();
+        painter->drawImage(0, 0, m_canvas);
+    }
+}
+
+void Layer::mousePressEvent(QMouseEvent *event)
+{
+    if (m_toolHandler) {
+        m_toolHandler->press(event);
+        update();
+    }
+}
+
+void Layer::mouseMoveEvent(QMouseEvent *event)
+{
+    if (m_toolHandler) {
+        m_toolHandler->move(event);
+        update();
+    }
+}
+
+void Layer::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (m_toolHandler) {
+        m_toolHandler->release(event);
+        update();
+    }
 }
 
 void Layer::setBackgroundColor(const QColor &color)
 {
     if (m_backgroundColor != color) {
         m_backgroundColor = color;
+        if (m_initialized) {
+            m_canvas.fill(m_backgroundColor);
+        }
         emit backgroundColorChanged();
         update();
     }
@@ -24,7 +82,33 @@ void Layer::setVisibility(const bool &visible)
 {
     if (m_visible != visible) {
         m_visible = visible;
-        visibleChanged();
+        emit visibleUpdated(visible);
+        update();
+    }
+}
+
+void Layer::setCurrentTool(ToolType type)
+{
+    if (m_currentTool != type) {
+        m_currentTool = type;
+
+        switch (m_currentTool) {
+        case ToolType::Brush:
+            m_toolHandler = std::make_unique<BrushTool>();
+            m_toolHandler->setImage(m_canvas);
+            m_toolHandler->setBrush(m_brush);
+
+            break;
+        case ToolType::Eraser:
+            m_toolHandler = std::make_unique<EraseTool>();
+            m_toolHandler->setImage(m_canvas);
+
+            break;
+        default:
+            m_toolHandler.reset();
+            break;
+        }
+
         update();
     }
 }
--- src/layer/Layer.hpp
+++ src/layer/Layer.hpp
@@ -1,31 +1,53 @@
 #pragma once
 
+#include "../brushes/BrushInfo.hpp"
 #include "../configs/precompiled.hpp"
+#include "Tools.hpp"
 
 namespace harmoniq { namespace layer {
 
-class Layer : public QQuickFramebufferObject
+class Layer : public QQuickPaintedItem
 {
         Q_OBJECT
 
         Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor NOTIFY backgroundColorChanged)
         Q_PROPERTY(bool isVisible READ isVisible WRITE setVisibility NOTIFY visibleChanged)
     public:
-        Layer() = default;
-        Renderer *createRenderer() const override;
+        explicit Layer(QQuickItem *parent = nullptr);
+        ~Layer();
 
         void setBackgroundColor(const QColor &color);
         void setVisibility(const bool &visible);
+        void setCurrentBrush(const brushes::Brush &brush);
+        void setCurrentTool(ToolType type);
 
         QColor backgroundColor() const;
         bool isVisible() const { return m_visible; }
+        brushes::Brush brush() const { return m_brush; }
+        QImage image() const { return m_canvas; }
+
+    protected:
+        void mousePressEvent(QMouseEvent *event) override;
+        void mouseMoveEvent(QMouseEvent *event) override;
+        void mouseReleaseEvent(QMouseEvent *event) override;
+        void paint(QPainter *painter) override;
+        void geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry) override;
+
     signals:
         void backgroundColorChanged();
-        void visibleChanged();
+        void visibleUpdated(bool visible);
 
     private:
-        QColor m_backgroundColor = QColor(255, 255, 255, 255);
+        void updateCanvasSize();
+
+        QImage m_canvas;
+        QColor m_backgroundColor;
+        brushes::Brush m_brush;
         bool m_visible = true;
+        ToolType m_currentTool = ToolType::None;
+        bool m_initialized = false;
+
+        std::unique_ptr<ToolHandler> m_toolHandler;
 };
 
 }} // namespace harmoniq::layer
