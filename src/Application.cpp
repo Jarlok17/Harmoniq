f7f0ca651bf2557dad9b2de59ba85c62d10e2f01
Jarlock17 <abilik48@gmail.com>
2025-03-30 15:59:39 +0300

    Modify Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,8 +1,10 @@
 #include <qsgrendererinterface.h>
 
-#include "brushes/BrushManager.hpp"
+// #include "brushes/BrushManager.hpp"
 #include "configs/precompiled.hpp"
+#include "layer/Layer.hpp"
 #include "layer/LayerManager.hpp"
+#include "layer/Tools.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -14,14 +16,15 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.addImportPath("qrc:/Styles");
-    // registre type layer
+
+    qmlRegisterUncreatableMetaObject(harmoniq::ToolWrapper::staticMetaObject, "harmoniq", 1, 0, "ToolType",
+                                     "Enum ToolType не може бути створений в QML");
     qmlRegisterType<harmoniq::layer::Layer>("Harmoniq_backend", 1, 0, "Layer");
 
-    // root context
-    harmoniq::layer::LayerManager layerManager;
-    harmoniq::brushes::BrushManager brushManager;
-    engine.rootContext()->setContextProperty("layerManager", &layerManager);
-    engine.rootContext()->setContextProperty("brushManager", &brushManager);
+    harmoniq::layer::LayerManager *layerManager = new harmoniq::layer::LayerManager();
+    // harmoniq::brushes::BrushManager *brushManager = new harmoniq::brushes::BrushManager();
+    engine.rootContext()->setContextProperty("layerManager", layerManager);
+    // engine.rootContext()->setContextProperty("brushManager", brushManager);
 
     const QUrl url(QStringLiteral("qrc:/Qml/main.qml"));
     QObject::connect(
